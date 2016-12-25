17cb7253f95346a1ad78481025da21075f089aae
superr <superr@ddayweb.com>
2016-12-25 13:10:42 -0600

    v1.1.2.9 - Updated SuperSU to v2.79-SR1. Added update-binary to support Nougat sparse_dat (Thanks @Captain_Throwback). Updated img2sdat by @xpirt to v1.1. Use the correct sparse_dat transfer list version for the ROM we are building. Replaced set_perm with set_metadata in custom zip updater-script. Use ifelse to unmount if mounted at the beginning of the updater-script.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#** SuperR's Kitchen v1.1.2.8 **#
+#** SuperR's Kitchen v1.1.2.9 **#
 ##by SuperR##
 
 ##**FEATURES**##
--- tools/img2sdat/img2sdat.py
+++ tools/img2sdat/img2sdat.py
@@ -3,13 +3,13 @@
 #====================================================
 #          FILE: img2sdat.py
 #       AUTHORS: xpirt - luxi78 - howellzhu
-#          DATE: 2016-11-23 16:20:11 CST
+#          DATE: 2016-12-23 17:46:55 CST
 #====================================================
 
 import sys, os, errno, tempfile
 import common, blockimgdiff, sparse_img
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 if sys.hexversion < 0x02070000:
     print >> sys.stderr, "Python 2.7 or newer is required."
@@ -26,7 +26,7 @@ try:
 except IndexError:
     print('Usage: img2sdat.py <system_img>\n')
     print('    <system_img>: input system image\n')
-    print("Visit xda thread for more information.\n")
+    print('Visit xda thread for more information.\n')
     try:
        input = raw_input
     except NameError: pass
@@ -34,13 +34,40 @@ except IndexError:
     sys.exit()
 
 def main(argv):
+    if len(sys.argv) < 3:
+        version = 4
+        item = True
+        while item:
+            print('''            1. Android Lollipop 5.0
+            2. Android Lollipop 5.1
+            3. Android Marshmallow 6.0
+            4. Android Nougat 7.0
+            ''')
+            item = raw_input('Choose system version: ')
+            if item == '1':
+                version = 1
+                break
+            elif item == '2':
+                version = 2
+                break
+            elif item == '3':
+                version = 3
+                break
+            elif item == '4':
+                version = 4
+                break
+            else:
+                return
+    else:
+        version = int(sys.argv[2])
+
     # Get sparse image
-    input_image = sparse_img.SparseImage(INPUT_IMAGE, tempfile.mkstemp()[1], '0')
-    
+    image = sparse_img.SparseImage(INPUT_IMAGE, tempfile.mkstemp()[1], '0')
+
     # Generate output files
-    b = blockimgdiff.BlockImageDiff(input_image, None)
+    b = blockimgdiff.BlockImageDiff(image, None, version)
     b.Compute('system')
-    
+
     print('Done! Output files: %s' % os.path.dirname(__file__))
     return
 
